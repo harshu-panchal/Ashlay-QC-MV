13eda3bd6da48cb8711939af8c76b7bc5659e9f6
Ayush Porwal <pa9067628@gmail.com>
2026-05-22 13:40:07 +0530

    delivery p-6

--- backend/app/modules/delivery/deliveryManager.js
+++ backend/app/modules/delivery/deliveryManager.js
@@ -1,8 +1,61 @@
 import { isDeliveryModuleEnabled } from "./deliveryFlags.js";
 import { getDeliveryProvider } from "./deliveryProviderRegistry.js";
+import { providerStatusToWorkflowStatus } from "./deliveryStatusMapping.js";
 
 export function getActiveDeliveryProviderName() {
   return getDeliveryProvider().name;
+}
+
+function ensureEnabledOrThrow() {
+  if (!isDeliveryModuleEnabled()) {
+    const err = new Error("Delivery module is disabled");
+    err.code = "DELIVERY_DISABLED";
+    throw err;
+  }
+}
+
+export async function createShipment(context) {
+  ensureEnabledOrThrow();
+  return getDeliveryProvider().createShipment(context);
+}
+
+export async function cancelShipment(context) {
+  ensureEnabledOrThrow();
+  return getDeliveryProvider().cancelShipment(context);
+}
+
+export async function getTrackingInfo(context) {
+  ensureEnabledOrThrow();
+  return getDeliveryProvider().getTrackingInfo(context);
+}
+
+export async function getETA(context) {
+  ensureEnabledOrThrow();
+  return getDeliveryProvider().getETA(context);
+}
+
+export async function getQuote(context) {
+  ensureEnabledOrThrow();
+  return getDeliveryProvider().getQuote(context);
+}
+
+export function mapProviderStatusToWorkflowStatus(providerName, providerStatus) {
+  return providerStatusToWorkflowStatus(providerName, providerStatus);
+}
+
+export function verifyWebhookSignature(rawBody, headers) {
+  ensureEnabledOrThrow();
+  return getDeliveryProvider().verifyWebhookSignature(rawBody, headers);
+}
+
+export function parseWebhookPayload(rawBody, headers) {
+  ensureEnabledOrThrow();
+  return getDeliveryProvider().parseWebhookPayload(rawBody, headers);
+}
+
+export async function refreshProviderToken() {
+  ensureEnabledOrThrow();
+  return getDeliveryProvider().refreshToken();
 }
 
 export async function emitDeliveryBroadcastForSeller(sellerId, payload) {
